8748a1b275f794d33b6b6626645ea8fd7078bf0d
loki <gunlinux@ya.ru>
2026-04-06 21:10:18 +0300

    new consumer

--- app/settings.py
+++ app/settings.py
@@ -5,8 +5,9 @@ _ = load_dotenv()
 
 # redis
 redis_url: str = os.environ.get("REDIS_URL", "redis://127.0.0.1/2")
+rabbit_url: str = os.environ.get("RABBIT_URL", "amqp://user:password@localhost:5672/")
+
 
 # Beer consumer
 BEER_URL: str = os.environ.get("BEER_URL", "http://127.0.0.1:6016/donate")
 BEER_STAT = "bs_donats"
-
